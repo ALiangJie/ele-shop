6d244bc9040905151e495c7579ffcc32fedaf793
liangjie <lj.liangjie@foxmail.com>
2022-06-24 01:35:58 +0800

    项目初始化，公用文件上传

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    lintOnSave: false,
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        https: false,
+        hot: false,
+        proxy: null
+    },
 })
