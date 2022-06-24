bc53d144553b86b44097f0b08837e8397d743fff
liangjie <lj.liangjie@foxmail.com>
2022-06-24 18:55:32 +0800

    商品排行组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,16 +3,19 @@
     <!--三级联动-->
     <TypeNav></TypeNav>
     <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
   </div>
 </template>
 
 <script>
 //引入其余组件
 import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
 
 export default {
   name: 'Home',
   components: {
+    TodayRecommend,
     ListContainer
   },
 }
